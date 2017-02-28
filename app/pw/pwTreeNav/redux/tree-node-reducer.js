6cb88f8734ab03018bc9a8c86e9017230ad2823d
Leif Frederiksen <lef@m.dk>
2017-02-28 15:29:49 +0100

    Deleted old tree view test.

--- app/pw/pwTreeNav/redux/tree-node-reducer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.treeNodeReducer = function (state, action) {
-    if (state === void 0) { state = []; }
-    switch (action.name) {
-        case 'LOAD_NODES':
-    }
-};
-//# sourceMappingURL=tree-node-reducer.js.map
--- app/pw/pwTreeNav/redux/tree-node-reducer.ts
+++
@@ -1,9 +0,0 @@
-import {TreeNode} from '../tree-node';
-export const treeNodeReducer = (state: any = [], action:any) => {
-  switch (action.name) {
-    case 'LOAD_NODES':
-    //   return state.nodes.map(n => {
-    //     return new TreeNode(n.key,n.url,n.name);
-    //   });
-  }
-};
